835377b4eeea7cf776d5033fbf1f58b4f23e91ef
Christopher Adams <christopher.m.adams2@gmail.com>
2024-05-13 09:46:31 -0700

    added placeMarker function and Players array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,19 +4,58 @@
 // event listener for start game button
 // get player name and marker choice
 
-const game = {turnCounter: 0, playerTurn: '', players: []};
-const playerName = "Etharialle";
-const playerMarker = "O";
+//const game = {turnCounter: 0, playerTurn: '', players: []};
+const playerNameTest = "Etharialle";
+const playerMarkerTest = "X";
 
-const getPlayers = (function () {
-    game.players.push({"name": playerName, "marker": playerMarker});
-    if (game.players[0].playerMarker === "X") {
-        game.players.push({"name": "computer", "marker": "O"});
+const Players = (function (playerName, playerMarker) {
+    let computerMarker;
+    if (playerMarker === "X") {
+        computerMarker = "O";
     } else {
-        game.players.push({"name": "computer", "marker": "X"});
+        computerMarker = "X";
     }
-})();
+    return [{"name": playerName, "marker": playerMarker}, {"name": "computer", "marker": computerMarker}];
+})(playerNameTest,playerMarkerTest);
+console.log(Players);
 
+let gameboard = Array(9);
+gameboard[3] = "X";
+console.log(gameboard);
+
+function placeMarker(gameboardCell, marker) {
+    if (!invalidCell(gameboardCell)) {
+        console.log("invalid placeMarker");
+    } else {
+        updateCell(gameboardCell, marker);
+    }
+    
+    
+}
+
+function invalidCell(gameboardCell) {
+    // Check for invalid cells
+    const availableCells = [];
+    for (let i = 0; i <= gameboard.length - 1; i++) {
+        if (!gameboard[i]) {
+            availableCells.push(i);
+        }
+    }
+    console.log(availableCells);
+    if (!availableCells.includes(gameboardCell)) {
+        console.log("invalid move, cell already picked");
+        //placeMarker(gameboardCell, marker);
+    }
+    return availableCells.includes(gameboardCell);
+}
+const updateCell = function (gameboardCell, marker) {
+    gameboard[gameboardCell] = marker;
+    console.log(gameboard);
+};
+
+console.log(placeMarker(3,"X"));
+
+/*
 function Gameboard() {
     const gameboardArray = [
         {"markerPlaced": ""},
@@ -45,7 +84,7 @@ function GameController() {
     const gameboardArray = Gameboard();
 
 }
-
+*/
 // O alwayas goes first
 //GameBoard.placeMarker()
 //Game
